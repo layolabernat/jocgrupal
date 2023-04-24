2888133b61cc4a640912a8551ab577d0c5951974
ferranRb <layolaalonsobernat@gmail.com>
2023-04-24 13:40:04 +0200

    Update Personatge animat.gd

--- Personatge animat.gd
+++ Personatge animat.gd
@@ -1,31 +1,37 @@
 extends KinematicBody2D
 
-# Propietats de moviment
 export var velocitat = 400
-export var gravetat = 400
-export var forcaSalt = 300
+<<<<<<< HEAD
+export var gravetat = 500
+=======
+export var gravetat = 450
+>>>>>>> b3e189a44ae05a6df693a0c08555ef3fee0e6a87
+export var forcaSalt = 275
 
 var velocitatX = Vector2(velocitat, 0)
 var velocitatY = Vector2.ZERO
 var estaAterra = false
 
 func _physics_process(delta):
-	# Capturar l'entrada d'usuari
-	var moviment = Input.get_action_strength("dreta") - Input.get_action_strength("esquerra")
+	
 	var salt = Input.is_action_pressed("ves_amunt")
 	
-	# Moure el personatge
-	velocitatX = Vector2(velocitat, 0) # Establir velocitat constant cap a la dreta
-	move_and_slide(velocitatX, Vector2.UP) # Moure el personatge
+
+	velocitatX = Vector2(velocitat, 0) 
+	move_and_slide(velocitatX, Vector2.UP) 
 	
-	# Saltar
+	
 	if estaAterra and salt:
 		velocitatY.y = -forcaSalt
 		estaAterra = false
 	
-	# Aplicar la gravetat i moure verticalment
+	
 	velocitatY.y += gravetat * delta
 	velocitatY = move_and_slide(velocitatY, Vector2.UP)
 	if is_on_floor():
 		estaAterra = true
+func _on_Area2D_body_entered(body):
+	if body.name == "personatge":
+		# Reinicia el joc
+		get_tree().reload_current_scene()
 
