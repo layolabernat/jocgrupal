f5e99798c3e87cfa37c04f536fadd7019a5eae7d
MateoLeCam <127502080+MateoLeCam@users.noreply.github.com>
2023-05-05 13:24:40 +0200

    quan et mors es para la musica

--- Area2D.gd
+++ Area2D.gd
@@ -9,10 +9,6 @@ func _process(delta):
 func _on_Area2D_body_entered(body):
 	if body.name == "personatge":
 		get_tree().reload_current_scene()
-
-
-
-
-
-
+	if body.name == "personatge":
+		get_tree().get_nodes_in_group("res://MusicPlayer.tscn")[0].volume.db = -80
 
