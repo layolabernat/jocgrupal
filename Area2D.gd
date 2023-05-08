08227a0638dd21169c534d396da0ec4e7ff6c433
ferranRb <layolaalonsobernat@gmail.com>
2023-05-08 13:39:55 +0200

    joc

--- Area2D.gd
+++ Area2D.gd
@@ -3,12 +3,12 @@ extends Area2D
 
 func _ready():
 	pass
-func _process(delta):
+func _process(delta): 
 	pass
 
 func _on_Area2D_body_entered(body):
 	if body.name == "personatge":
 		get_tree().reload_current_scene()
 	if body.name == "personatge":
-		get_tree().get_nodes_in_group("res://MusicPlayer.tscn")[0].volume.db = -80
+		MusicPlayer.volume_db = -80
 
