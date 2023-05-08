1cc38b0abc82982b6dfd2d721dfb386e41ceb130
MateoLeCam <127502080+MateoLeCam@users.noreply.github.com>
2023-05-08 14:13:18 +0200

    quan es mor para la música i quan reviu torna a sonar

--- Area2D.gd
+++ Area2D.gd
@@ -8,6 +8,6 @@ func _process(delta):
 
 func _on_Area2D_body_entered(body):
 	if body.name == "personatge":
-		get_tree().reload_current_scene()
-	if body.name == "personatge":
 		MusicPlayer.stop()
+		get_tree().reload_current_scene()
+		
